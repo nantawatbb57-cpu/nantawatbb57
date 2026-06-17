d5ca3c3f6b62f4298465bccbba0948b2e4a5f54f
nantawatbb57-cpu <nantawatbb57@gmail.com>
2026-06-17 12:12:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
 document.addEventListener('DOMContentLoaded', function () 
-                          https://cdn.tailwindcss.com{
+                          {
     console.log('Portfolio ของ NANTAWAT พร้อมแล้ว!');
 });
